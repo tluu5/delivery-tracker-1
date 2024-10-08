036ce521f4fa3ca6adf9b607e2df50a4675685a3
Thieu Luu <96815689+tluu5@users.noreply.github.com>
2024-10-08 20:13:04 +0000

    working delivery tracker

--- app/controllers/deliveries_controller.rb
+++ app/controllers/deliveries_controller.rb
@@ -21,13 +21,14 @@ class DeliveriesController < ApplicationController
     the_delivery = Delivery.new
     the_delivery.description = params.fetch("query_description")
     the_delivery.supposed_to_arrive_on = params.fetch("query_supposed_to_arrive_on")
-    the_delivery.user_id = params.fetch("query_user_id")
     the_delivery.details = params.fetch("query_details")
-    the_delivery.arrived = params.fetch("query_arrived", false)
+    the_delivery.arrived = false
+
+    the_delivery.user_id = current_user.id
 
     if the_delivery.valid?
       the_delivery.save
-      redirect_to("/deliveries", { :notice => "Delivery created successfully." })
+      redirect_to("/deliveries", { :notice => "Added to list" })
     else
       redirect_to("/deliveries", { :alert => the_delivery.errors.full_messages.to_sentence })
     end
@@ -36,18 +37,13 @@ class DeliveriesController < ApplicationController
   def update
     the_id = params.fetch("path_id")
     the_delivery = Delivery.where({ :id => the_id }).at(0)
-
-    the_delivery.description = params.fetch("query_description")
-    the_delivery.supposed_to_arrive_on = params.fetch("query_supposed_to_arrive_on")
-    the_delivery.user_id = params.fetch("query_user_id")
-    the_delivery.details = params.fetch("query_details")
-    the_delivery.arrived = params.fetch("query_arrived", false)
+    the_delivery.arrived = true
 
     if the_delivery.valid?
       the_delivery.save
-      redirect_to("/deliveries/#{the_delivery.id}", { :notice => "Delivery updated successfully."} )
+      redirect_to("/deliveries", { :notice => "Delivery updated successfully."} )
     else
-      redirect_to("/deliveries/#{the_delivery.id}", { :alert => the_delivery.errors.full_messages.to_sentence })
+      redirect_to("/deliveries", { :alert => the_delivery.errors.full_messages.to_sentence })
     end
   end
 
